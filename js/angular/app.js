e13484573efee6ea61e9dd812947d2de710c1f74
Alessio Caiazza <nolith@abisso.org>
2013-05-28 16:44:56 +0200

    todo example from angular.js

--- js/angular/app.js
+++ js/angular/app.js
@@ -6,5 +6,6 @@ angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/view1', {templateUrl: 'static/partials/partial1.html', controller: 'MyCtrl1'});
     $routeProvider.when('/view2', {templateUrl: 'static/partials/partial2.html', controller: 'MyCtrl2'});
+    $routeProvider.when('/todo', {templateUrl: 'static/partials/todo.html', controller: 'TodoCtrl'});
     $routeProvider.otherwise({redirectTo: '/view1'});
   }]);
